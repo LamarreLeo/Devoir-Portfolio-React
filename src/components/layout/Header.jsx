0d77394cf7ca3757095402781fa24507acf32c98
Leo.Lamarre <leo.lamarre24@gmail.com>
2025-08-12 23:57:26 +0200

    feat: import font

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 function Header() {
-  return <header></header>;
+  return (
+    <header className="bg-gray-800 h-16  text-white uppercase px-8 flex items-center justify-between">
+      <NavLink
+        to="/"
+        className={({ isActive }) => (isActive ? 'active' : undefined)}
+      >
+        John Doe
+      </NavLink>
+    </header>
+  );
 }
 
 export default Header;
