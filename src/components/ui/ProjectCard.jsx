0d0f6a1a48a9d160c74440d6208d15e726c31442
Leo.Lamarre <leo.lamarre24@gmail.com>
2025-08-19 01:48:56 +0200

    fix: stylling of ProjectCard

--- src/components/ui/ProjectCard.jsx
+++ src/components/ui/ProjectCard.jsx
@@ -1,20 +1,21 @@
 function ProjectCard({ title, description, techno, picture, slug }) {
   return (
-    <div>
+    <div id={slug} className="rounded-2xl shadow-md overflow-hidden flex flex-col text-center border border-gray-300">
       <img
-        src={`assets/img/${picture}`}
-        alt="Image représentant le projet ${title}"
+        src={`assets/img/portfolio/${picture}`}
+        alt={`Image représentant le projet "${title}"`}
+        className="h-56 object-cover"
       />
 
-      <div>
-        <h2>{title}</h2>
-        <p>{description}</p>
+      <div className="p-2">
+        <h2 className="text-xl">{title}</h2>
+        <p className="mb-2">{description}</p>
 
-        <a href={`/portfolio/${slug}`}>Voir le site</a>
+        <a href={`/portfolio#${slug}`} className="inline-block bg-blue-500 text-white px-4 py-2 mb-2 rounded-lg hover:bg-blue-600">Voir le site</a>
       </div>
 
-      <div>
-        <span>{techno}</span>
+      <div className="bg-gray-200 py-2 border border-gray-300">
+        <span className="text-sm">{techno}</span>
       </div>
     </div>
   );
