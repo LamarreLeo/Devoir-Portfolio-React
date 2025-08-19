ed096eec240598775d0000de7a511eb6afceb419
Leo.Lamarre <leo.lamarre24@gmail.com>
2025-08-19 14:54:16 +0200

    Final ProjectCard commit

--- src/components/ui/ProjectCard.jsx
+++ src/components/ui/ProjectCard.jsx
@@ -1,31 +1,35 @@
 function ProjectCard({ title, description, techno, picture, slug }) {
   return (
-    <div
+    <article
       id={slug}
-      className="rounded-2xl shadow-md overflow-hidden flex flex-col text-center border border-gray-300"
+      className="rounded-2xl overflow-hidden flex flex-col text-center border border-gray-300 
+      hover:shadow-md hover:bg-[#efefef] transition duration-300 ease-in-out"
     >
+      {/* Image */}
       <img
         src={`assets/img/portfolio/${picture}`}
-        alt={`Image représentant le projet "${title}"`}
+        alt={`Aperçu du projet "${title}"`}
         className="h-56 object-cover"
       />
 
+      {/* Content */}
       <div className="p-2 flex-1 flex flex-col">
         <h2 className="text-xl">{title}</h2>
         <p className="mb-2">{description}</p>
 
         <a
           href={`/portfolio#${slug}`}
-          className="inline-block bg-blue-500 text-white px-4 py-2 mb-2 rounded-lg hover:bg-blue-600 mt-auto mx-auto"
+          className="inline-block bg-blue-600 text-white px-4 py-2 mb-2 rounded-lg hover:bg-blue-500 mt-auto mx-auto"
         >
-          Voir le site
+          Voir le site <span className="sr-only">du projet {title}</span>
         </a>
       </div>
 
-      <div className="bg-gray-200 py-2 border-t border-gray-300">
+      {/* Footer */}
+      <footer className="bg-[#efefef] py-2 border-t border-gray-300">
         <span className="text-sm">{techno}</span>
-      </div>
-    </div>
+      </footer>
+    </article>
   );
 }
 
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -4,6 +4,7 @@ import projectsData from '../assets/data/projects.json';
 function Portfolio() {
   return (
     <main className="mt-12">
+      {/* Projects List */}
       <section className="px-6 py-12 max-w-7xl mx-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {projectsData.projects.map((project) => (
