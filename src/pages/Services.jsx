ec3e0324a79666b04c858ce5d162c896a639891e
Leo.Lamarre <leo.lamarre24@gmail.com>
2025-08-22 00:18:42 +0200

    feat: services page implementation

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -3,8 +3,18 @@ import { Brush, CodeXml, Search } from 'lucide-react';
 function Services() {
   return (
     <main className="mt-12">
+      <img src="/assets/img/banner.jpg" alt="" loading="lazy" />
+
+      {/* Services Section */}
       <section className="px-6 py-12 max-w-7xl mx-auto">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+        <div className="text-center flex flex-col gap-2">
+          <h1 className="text-4xl">Mon offre de services</h1>
+          <p>Voici les prestations sur lesquelles je peux intervenir</p>
+          <div className="h-1 bg-blue-600 mx-auto w-32 lg:w-112 mt-6"></div>
+        </div>
+
+        {/* Services List */}
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mt-16">
           <ServiceCard
             Icon={
               <Brush
