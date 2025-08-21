3db0674a3b25015dc15cf72ca594e0d0a43569a3
Leo.Lamarre <leo.lamarre24@gmail.com>
2025-08-21 20:18:24 +0200

    feat: HomePage integration

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,12 +12,64 @@ function Home() {
   }, []);
 
   return (
-    <main className="mt-12">
-      {userData ? (
-        <Modale userData={userData} />
-      ) : (
-        <p>Chargement des données GitHub...</p>
-      )}
+    <main className="">
+      {/* Hero */}
+      <section className="bg-[url('/assets/img/hero-bg.jpg')] bg-cover bg-center h-screen flex flex-col justify-center text-center text-white">
+        <div
+          className="absolute inset-0 bg-gray-600/50"
+          aria-hidden="true"
+        ></div>
+
+        <div className="flex flex-col gap-6 p-6 mt-12 z-10">
+          <h1 className="text-5xl leading-16">Bonjour, je suis John Doe</h1>
+          <h2 className="text-4xl">Développeur web full stack</h2>
+
+          {userData ? (
+            <Modale userData={userData} />
+          ) : (
+            <p role="status">Chargement des données GitHub...</p>
+          )}
+        </div>
+      </section>
+
+      <div className="bg-gray-100 px-6 py-12 max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2">
+        {/* About */}
+        <section className="bg-white p-6">
+          <h3 className="text-2xl pb-4 border-b-4 border-blue-600">A propos</h3>
+          <div className='flex flex-col gap-4'>
+            <img
+              src="/assets/img/john-doe-about.jpg"
+              alt="Image de John Doe"
+              className="mt-12"
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio,
+              necessitatibus conselectur tempore perferendis nostrum, ex
+              delectus reiciendis impedit aut iure enim placeat? Natus, neque
+              at?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio,
+              necessitatibus conselectur tempore perferendis nostrum, ex
+              delectus reiciendis impedit aut iure enim placeat? Natus, neque
+              at?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio,
+              necessitatibus conselectur tempore perferendis nostrum, ex
+              delectus reiciendis impedit aut iure enim placeat? Natus, neque
+              at?
+            </p>
+          </div>
+        </section>
+
+        {/* Skills */}
+        <section className="bg-white p-6">
+          <h3 className="text-2xl pb-4 border-b-4 border-blue-600">
+            Mes compétences
+          </h3>
+        </section>
+      </div>
     </main>
   );
 }
