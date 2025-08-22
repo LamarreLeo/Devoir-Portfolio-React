8a66c464c29cf533c762fc431a985b6e4c529b8e
Leo.Lamarre <leo.lamarre24@gmail.com>
2025-08-22 03:40:24 +0200

    Basic contact page structure

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,31 @@
 function Contact() {
-  return <main className="mt-12"></main>;
+  return (
+    <main className="mt-12">
+      <div className="w-full bg-gray-100 py-12 px-3">
+        <div className="text-center flex flex-col gap-2">
+          <h1 className="text-4xl">Mon offre de services</h1>
+          <p>Voici les prestations sur lesquelles je peux intervenir</p>
+          <div className="h-1 bg-blue-600 mx-auto w-32 lg:w-112 mt-6 mb-16"></div>
+        </div>
+
+        <div className="mx-auto max-w-7xl grid grid-cols-1 md:grid-cols-2 bg-white rounded-2xl shadow-2xl">
+          {/* Contact Form */}
+          <section className="p-6">
+            <h2 className="text-2xl pb-4 border-b-4 border-blue-600">
+              Formulaire de contact
+            </h2>
+          </section>
+
+          {/* Contact Informations */}
+          <section className="p-6">
+            <h2 className="text-2xl pb-4 border-b-4 border-blue-600">
+              Mes coordonnées
+            </h2>
+          </section>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default Contact;
