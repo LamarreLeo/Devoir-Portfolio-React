f1b49682840b9cfd96adce547ccd67f95b63ef61
Leo.Lamarre <leo.lamarre24@gmail.com>
2025-08-22 05:00:12 +0200

    Adding contact information

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,5 @@
 import ContactForm from '../components/forms/ContactForm';
+import { Map, MapPin, Smartphone, Mail } from 'lucide-react';
 
 function Contact() {
   return (
@@ -28,6 +29,37 @@ function Contact() {
             <h2 className="text-2xl pb-4 border-b-4 mb-12 border-blue-600">
               Mes coordonnées
             </h2>
+
+            <address className="flex flex-col gap-4 not-italic">
+              <ul className="flex flex-col gap-2">
+                <li>
+                  <h3 className="text-xl">John Doe</h3>
+                </li>
+                <li className="flex items-center">
+                  <Map className="inline-block mr-1" strokeWidth={1} />
+                  40 rue Laure DieBold
+                </li>
+
+                <li className="flex items-center">
+                  <MapPin className="inline-block mr-1" strokeWidth={1} />
+                  69009 Lyon, France
+                </li>
+
+                <li className="flex items-center">
+                  <a href="tel:+33123456789">
+                    <Smartphone className="inline-block mr-1" strokeWidth={1} />
+                    01 23 45 67 89
+                  </a>
+                </li>
+
+                <li className="flex items-center">
+                  <a href="mailto:john.doe@gmail.com">
+                    <Mail className="inline-block mr-1" strokeWidth={1} />
+                    john.doe@gmail.com
+                  </a>
+                </li>
+              </ul>
+            </address>
           </section>
         </div>
       </div>
