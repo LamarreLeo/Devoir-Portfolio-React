51de661a64b888a1846ef04df9db8efc349699b5
Leo.Lamarre <leo.lamarre24@gmail.com>
2025-08-22 15:53:37 +0200

    Adding Helmet for the title and meta noindex

--- src/pages/MentionsLegales.jsx
+++ src/pages/MentionsLegales.jsx
@@ -1,17 +1,24 @@
 import LegalAccordion from '../components/ui/LegalAccordion';
+import { Helmet } from 'react-helmet';
 
 function MentionsLegales() {
   return (
-    <main className="mt-12">
-      <section className="px-6 py-12 max-w-7xl mx-auto">
-        <div className="text-center flex flex-col gap-2">
-          <h1 className="text-4xl">Mentions légales</h1>
-          <div className="h-1 bg-blue-600 mx-auto w-32 lg:w-112 mt-6 mb-16"></div>
-        </div>
+    <>
+      <Helmet>
+        <title>Mentions légales</title>
+        <meta name="robots" content="noindex, nofollow" />
+      </Helmet>
+      <main className="mt-12">
+        <section className="px-6 py-12 max-w-7xl mx-auto">
+          <div className="text-center flex flex-col gap-2">
+            <h1 className="text-4xl">Mentions légales</h1>
+            <div className="h-1 bg-blue-600 mx-auto w-32 lg:w-112 mt-6 mb-16"></div>
+          </div>
 
-        <LegalAccordion />
-      </section>
-    </main>
+          <LegalAccordion />
+        </section>
+      </main>
+    </>
   );
 }
 
