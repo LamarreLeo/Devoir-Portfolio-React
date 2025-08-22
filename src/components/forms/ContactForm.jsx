8ec49f29c8f7978691c1d9c22c512f9bb82679f1
Leo.Lamarre <leo.lamarre24@gmail.com>
2025-08-22 04:12:10 +0200

    ContactForm integration

--- src/components/forms/ContactForm.jsx
+++ src/components/forms/ContactForm.jsx
@@ -1,5 +1,57 @@
 function ContactForm() {
-  return <form action=""></form>;
+  return (
+    <form action="" method="post" className="flex flex-col gap-4">
+      <input
+        type="text"
+        id="name"
+        name="name"
+        placeholder="Votre nom"
+        autoComplete="name"
+        required
+        className="border border-gray-300 rounded-lg p-2.5"
+      />
+      <input
+        type="email"
+        id="email"
+        name="email"
+        placeholder="Votre adresse email"
+        autoComplete="email"
+        required
+        className="border border-gray-300 rounded-lg p-2.5"
+      />
+      <input
+        type="tel"
+        id="phone"
+        name="phone"
+        placeholder="Votre numéro de téléphone"
+        autoComplete="tel"
+        required
+        className="border border-gray-300 rounded-lg p-2.5"
+      />
+      <input
+        type="text"
+        id="subject"
+        name="subject"
+        placeholder="Sujet"
+        required
+        className="border border-gray-300 rounded-lg p-2.5"
+      />
+      <textarea
+        id="message"
+        name="message"
+        placeholder="Votre message"
+        required
+        className="border border-gray-300 rounded-lg p-2.5 h-60"
+      ></textarea>
+
+      <button
+        type="submit"
+        className="cursor-pointer text-white bg-blue-600 hover:bg-blue-700 px-4 py-2 rounded-lg"
+      >
+        Envoyer
+      </button>
+    </form>
+  );
 }
 
 export default ContactForm;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,24 +1,31 @@
+import ContactForm from '../components/forms/ContactForm';
+
 function Contact() {
   return (
     <main className="mt-12">
       <div className="w-full bg-gray-100 py-12 px-3">
         <div className="text-center flex flex-col gap-2">
-          <h1 className="text-4xl">Mon offre de services</h1>
-          <p>Voici les prestations sur lesquelles je peux intervenir</p>
+          <h1 className="text-4xl">Contact</h1>
+          <p>
+            Pour me contacter en vue d'un entretien ou d'une future
+            collaboration, merci de remplir le formulaire de contact.
+          </p>
           <div className="h-1 bg-blue-600 mx-auto w-32 lg:w-112 mt-6 mb-16"></div>
         </div>
 
         <div className="mx-auto max-w-7xl grid grid-cols-1 md:grid-cols-2 bg-white rounded-2xl shadow-2xl">
           {/* Contact Form */}
           <section className="p-6">
-            <h2 className="text-2xl pb-4 border-b-4 border-blue-600">
+            <h2 className="text-2xl pb-4 border-b-4 mb-12 border-blue-600">
               Formulaire de contact
             </h2>
+
+            <ContactForm />
           </section>
 
           {/* Contact Informations */}
           <section className="p-6">
-            <h2 className="text-2xl pb-4 border-b-4 border-blue-600">
+            <h2 className="text-2xl pb-4 border-b-4 mb-12 border-blue-600">
               Mes coordonnées
             </h2>
           </section>
